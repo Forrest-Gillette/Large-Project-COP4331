44a0f61a7ee658a916a32b0d68e4ab67cc7ca49b
SonnyLiuu <sonnyliu0213@gmail.com>
2023-07-14 19:18:46 -0400

    Working React App login/register

---
+++ server.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const registerRoutes = require("./routes/registerAPI");
+const loginRoutes = require("./routes/loginAPI");
+const forgotRoutes = require("./routes/forgotAPI");
+
+
+// express app
+const app = express();
+
+
+// middleware
+app.use(express.static(path.join(__dirname + "/frontend/build")));
+// AFTER defining routes: Anything that doesn't match what's above, 
+// send back server.html;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/server.html'))
+})
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/register", registerRoutes);
+app.use("/api/login", loginRoutes);
+app.use("/api/forgot", forgotRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  })
+}
+
+const port = process.env.PORT || 2500;
+app.listen(port, console.log(`Listening on port ${port}...`));
+
+
+const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, connectionParams)
+.then(() => {
+  // listen for requests
+  // app.listen(port, () => {
+  console.log("connected to the db and listening on port", process.env.PORT)
+  // })
+})
+.catch((error) => {
+  console.log("Could not connect to database!");
+  console.log(error)
+})
---
+++ util/sendEmailAPI.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (email, subject, text) => {
+  try {
+    if (!email) {
+        throw new Error("No recipient email specified");
+    }
+
+    const transport = nodemailer.createTransport({
+      host: process.env.HOST,
+      service: process.env.SERVICE,
+      port: Number(process.env.EMAIL_PORT),
+      secure: process.env.SECURE === 'true',
+      auth: {
+        user: process.env.ADMIN,
+        pass: process.env.PASS,
+      },
+    });
+
+    await transport.sendMail({
+      from: process.env.ADMIN,
+       to: 'tyler.beach@ucf.edu',
+      // to: email,
+      subject: subject,
+      text: text,
+    });
+
+    console.log("Email sent!");
+  } catch (error) {
+    console.log("Email not sent :(");
+    console.log(error);
+  }
+};
